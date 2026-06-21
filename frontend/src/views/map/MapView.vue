c5b09eece3064268607193dff5c434ec0f09e33d
sherry-107 <chaseoyoung13@gmail.com>
2026-06-21 20:47:26 +0900

    🌱feat : 은행 검색 기능 추가

--- frontend/src/views/map/MapView.vue
+++ frontend/src/views/map/MapView.vue
@@ -1,12 +1,22 @@
-<!-- views/map/MapView.vue -->
 <template>
   <div class="map-view">
-    <BankMap />
-    <BankList />
+    <BankList @search-result="onSearchResult" @select-bank="onSelectBank" />
+    <BankMap :banks="searchedBanks" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import BankMap from '@/components/map/BankMap.vue'
 import BankList from '@/components/map/BankList.vue'
+
+const searchedBanks = ref([])
+
+const onSearchResult = (banks) => {
+  searchedBanks.value = banks
+}
+
+const onSelectBank = (bank) => {
+  // 추후: 지도 중심을 해당 은행으로 이동시키는 기능 등 추가 가능
+}
 </script>
